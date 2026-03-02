5d28cde3aeb7227c3ac54b4c96d8cbd7c29157d8
IlonaChupa <ilonachupa@gmail.com>
2026-03-02 17:46:51 +0200

    done

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -1,10 +1,11 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
 import java.util.List;
 
-public interface MachineService <Machine> {
+public interface MachineService <T extends Machine> {
 
-    List<Machine> getAll(Class <? extends Machine> type);
+    List<T> getAll(Class <? extends Machine> type);
 
     <T> void fill(List<? super T> machines, T value);
 
