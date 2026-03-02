f241bb7cc8feeb74297d86508f41303f055e00ff
IlonaChupa <ilonachupa@gmail.com>
2026-03-02 17:35:50 +0200

    done

--- src/main/java/core/mate/academy/service/TruckProducer.java
+++ src/main/java/core/mate/academy/service/TruckProducer.java
@@ -1,6 +1,6 @@
 package core.mate.academy.service;
-import core.mate.academy.model.Truck;
 
+import core.mate.academy.model.Truck;
 import java.util.List;
 
 public class TruckProducer implements MachineProducer<Truck> {
